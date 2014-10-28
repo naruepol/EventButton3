f454d01d47fdec7c6df0c464aae299267d979d0b
naruepol <naruapon@gmail.com>
2014-10-29 01:37:03 +0700

    First Commit

---
+++ src/com/example/eventbutton3/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.eventbutton3;
+
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity implements OnClickListener  {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        Button b = (Button) findViewById(R.id.button1);
+        b.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		if(v.getId() == R.id.button1){
+			Toast.makeText(MainActivity.this, "Button Click3!", Toast.LENGTH_SHORT).show();	
+		}
+	}
+    
+}
